0c6381486db43c7b963fb5e1638f72d86879858b
vinicius-hso <viniciushso@icloud.com>
2021-10-16 18:18:15 -0300

    Fisrt commit

---
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -0,0 +1,32 @@
+import { styled } from "@mui/material/styles";
+
+export const PageTitleContainer = styled("div")`
+  margin: ${({ theme }) =>
+    theme.spacing(5) +
+    " " +
+    "0"}; // base 8, total 5x8 pixels = 40 pixels -|- margin: 40px 0;
+  text-align: center;
+`;
+
+export const PageTitleStyled = styled("h2")`
+  margin: 0;
+  color: ${({ theme }) => theme.palette.primary.main};
+  font-size: ${({ theme }) => theme.typography.h6.fontSize};
+  font-weight: 600;
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  }
+`;
+
+export const PageSubtitleStyled = styled("h3")`
+  margin: ${({ theme }) =>
+    theme.spacing(1.5) + " " + "0"}; // base 9, 1.5x8 = 12px
+  color: ${({ theme }) => theme.palette.text.primary};
+  font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  font-weight: normal;
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    font-size: ${({ theme }) => theme.typography.body2.fontSize};
+  }
+`;
